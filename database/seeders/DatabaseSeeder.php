c156500fdde57e3a3f2c79c18f88be981532465c
yenilikci <melihcelik017@outlook.com>
2020-11-04 23:19:50 +0300

    Yazılar (view,controller) ve config=>tr

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(CategorySeeder::class);
+        $this->call(ArticleSeeder::class);
         // \App\Models\User::factory(10)->create();
     }
 }
